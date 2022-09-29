d85e57614f3253fbf07c54c92e3f75e6cd218b5d
GuiParpineli <guiparpineli@hotmail.com>
2022-09-28 21:35:23 -0300

    refatoring

--- app/src/main/java/com/teste/drinkwater/MainActivity.kt
+++ app/src/main/java/com/teste/drinkwater/MainActivity.kt
@@ -15,11 +15,11 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var edit_peso: EditText
-    private lateinit var edit_idade: EditText
-    private lateinit var bt_calcular: Button
-    private lateinit var txt_resultadoml: TextView
-    private lateinit var ic_redefinir_dados: ImageView
+    private lateinit var editPeso: EditText
+    private lateinit var editIdade: EditText
+    private lateinit var btCalcular: Button
+    private lateinit var txtResultadoml: TextView
+    private lateinit var icRedefinirDados: ImageView
 
     private lateinit var calcularIngestaoDiaria: CalcularIngestaoDiaria
 
@@ -29,49 +29,50 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         supportActionBar!!.hide()
-        IniciarComponentes()
+        iniciarComponentes()
         calcularIngestaoDiaria = CalcularIngestaoDiaria()
 
-        bt_calcular.setOnClickListener {
-            if (edit_peso.text.toString().isEmpty()) {
+        btCalcular.setOnClickListener {
+            if (editPeso.text.toString().isEmpty()) {
                 Toast.makeText(this, R.string.toast_informe_peso, Toast.LENGTH_SHORT).show()
-            } else if (edit_idade.text.toString().isEmpty()) {
+            } else if (editIdade.text.toString().isEmpty()) {
                 Toast.makeText(this, R.string.toast_informe_idade, Toast.LENGTH_SHORT).show()
             } else {
-                val peso = edit_peso.text.toString().toDouble()
-                var idade = edit_idade.text.toString().toInt()
+                val peso = editPeso.text.toString().toDouble()
+                var idade = editIdade.text.toString().toInt()
                 calcularIngestaoDiaria.CalcularTotalMl(peso, idade)
                 val formatar = NumberFormat.getNumberInstance(Locale("pt", "BR"))
                 formatar.isGroupingUsed = false
-                txt_resultadoml.text =
-                    "${formatar.format(calcularIngestaoDiaria.ResultMl()).toString()} ml"
+                txtResultadoml.text =
+                    "${formatar.format(calcularIngestaoDiaria.ResultMl())} ml"
             }
         }
 
-        ic_redefinir_dados.setOnClickListener {
+        icRedefinirDados.setOnClickListener {
 
             var alertDialog = AlertDialog.Builder(this)
             alertDialog
                 .setTitle(R.string.dialog_title)
                 .setMessage(R.string.dialog_desc)
-                .setPositiveButton("Ok", { dialogInterface, i ->
-                    edit_peso.setText("")
-                    edit_idade.setText("")
-                    txt_resultadoml.text = ""
-                })
-            alertDialog.setNegativeButton("Cancelar", { dialogInterface, i -> })
+                .setPositiveButton("Ok",
+                    { _, _ ->
+                        editPeso.setText("")
+                        editIdade.setText("")
+                        txtResultadoml.text = ""
+                    })
+            alertDialog.setNegativeButton("Cancelar", { _, _ -> })
             val dialog = alertDialog.create()
             dialog.show()
         }
 
     }
 
-    private fun IniciarComponentes() {
-        edit_peso = findViewById(R.id.edit_peso)
-        edit_idade = findViewById(R.id.edit_idade)
-        bt_calcular = findViewById(R.id.bt_calcular)
-        txt_resultadoml = findViewById(R.id.txt_tesultado_ml)
-        ic_redefinir_dados = findViewById(R.id.ic_redefinir)
+    private fun iniciarComponentes() {
+        editPeso = findViewById(R.id.edit_peso)
+        editIdade = findViewById(R.id.edit_idade)
+        btCalcular = findViewById(R.id.bt_calcular)
+        txtResultadoml = findViewById(R.id.txt_tesultado_ml)
+        icRedefinirDados = findViewById(R.id.ic_redefinir)
     }
 
 }
